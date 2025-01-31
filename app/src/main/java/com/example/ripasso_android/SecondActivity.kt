2e8e5462b2e7a1e0a50e01c3cfc2e0526393065f
arezz <davide.arezzi@edu.itspiemonte.it>
2025-01-31 11:52:30 +0100

    first commit

--- app/src/main/java/com/example/ripasso_android/SecondActivity.kt
+++ app/src/main/java/com/example/ripasso_android/SecondActivity.kt
@@ -1,20 +1,26 @@
 package com.example.ripasso_android
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ripasso_android.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_second)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.secondobutton.setOnClickListener{
+            val intent = Intent (this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
         }
+
     }
 }
